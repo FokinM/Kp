1fa11b6f47697371d74e8e14d6df5c14e958a381
FokinM <legolas10121999@mail.ru>
2021-09-10 17:15:33 +0800

    dz

---
+++ Lab_1.4/Lab_1.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab_1._4
+{
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=1;
+            if (args.Length > 0)
+            {
+                foreach (string str in args)
+                {
+                    a += Convert.ToInt32(str);
+                    b *= Convert.ToInt32(str);
+                }
+            }
+
+            
+
+            Console.WriteLine("Сумма = " + a);
+            Console.WriteLine("Произведение = " + b);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab_1/Lab_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_1
+{
+
+    class Password
+    {
+        public string pas = "dfggde2";
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Password pas_1 = new Password();
+
+            Console.WriteLine("Введите пароль:");
+            while (pas_1.pas != Console.ReadLine())
+            {
+                Console.WriteLine("не совпал");
+            }
+            Console.WriteLine("Cовпал");
+
+            Console.ReadLine();
+        }
+    }
+}
